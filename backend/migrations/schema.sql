b5e462a3bb3f380c3076299d8bee523f74833191
SIRU TAN <siru0915@gmail.com>
2026-07-14 01:22:31 -0400

    working on backend

---
+++ backend/migrations/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
