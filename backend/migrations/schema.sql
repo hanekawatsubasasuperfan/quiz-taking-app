608ae101c7dc330227a804a52ea8b5ae91bdbb35
SIRU TAN <siru0915@gmail.com>
2026-07-31 00:09:28 -0400

    added schema for quizzes and questions

--- backend/migrations/schema.sql
+++ backend/migrations/schema.sql
@@ -13,3 +13,10 @@ user_id INT NOT NULL REFERENCES users(id),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    quiz_id INT NOT NULL REFERENCES quizzes(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
